f3f6776a9eae932c5e7eeb83fb6894e71f1c78bd
Pedro Lima <tsc.pedrolima@gmail.com>
2023-08-13 22:03:49 -0300

    style: add header, main, card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+
 function App() {
 
 	return (
 		<>
-			<h1>Hello</h1>
+			<Header />
+			<Main />
 		</> 
 	);
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+function Header() {
+	return (
+		<div className="container bg-header" id="header">
+			<div className="row">
+				<div className="col">
+					<h1></h1>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
